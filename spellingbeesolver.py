be75b9569d0af90346948ed0efa219ef4092b9e1
zachm01 <62446698+zachm01@users.noreply.github.com>
2022-11-09 12:49:31 -0500

    Added comments

--- spellingbeesolver.py
+++ spellingbeesolver.py
@@ -1,16 +1,13 @@
-# Previous NYT Spelling Bee letters https://www.sbsolver.com/archive
+str_to_chars = lambda str: [char for char in str] # for ease of entering allowed letters
 
-str_to_chars = lambda str: [char for char in str]
-
-with open('data/words_alpha.txt', 'r') as f:
+with open('words_alpha.txt', 'r') as f:   # name of your words file
     lines = f.readlines()
 
 for i in range(len(lines)):
     lines[i] = lines[i].replace('\n', '')
 
-allowed_letters = str_to_chars('oaflmru') # first letter of the string is the center letter of the hexagon
+allowed_letters = str_to_chars('oaflmru') # first letter of the string is the center letter of the hexagon on the NYT website/app
 good_words = []
-panagrams = []
 
 for i in range(len(lines)):
     num_allowed_letters = 0
@@ -21,7 +18,7 @@ for i in range(len(lines)):
     if num_allowed_letters == len(lines[i]) and len(lines[i]) >= 4 and allowed_letters[0] in lines[i]:
         good_words.append(lines[i])
 
-print(f'\nFound {len(good_words)} matches out of 370105 words\n')
+print(f'\nFound {len(good_words)} matches out of {len(lines)} words\n')
 
 for i in enumerate(good_words):
-    print(f'{i[0] + 1}\t{i[1]}')
+    print(f'{i[0]+1}\t{i[1]}')
